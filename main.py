a6986de03acdb66a07aeaa6dd8a81d96dd150a02
SebastianFiser <sebastianfiser@gmail.com>
2025-10-28 21:30:28 +0100

    complete finish of this project, wont update

--- main.py
+++ main.py
@@ -1,4 +1,45 @@
+#!/usr/bin/env python3
+import os
+import sys
+import speech_recognition as sr
+import ctypes
+import ctypes.util
 import subprocess
 
-text = input("Enter text to speak: ")
-subprocess.run(['espeak-ng', '-v', 'en+ja', '-s 150', text])
+# clearing ALSA a JACK lines
+libc = ctypes.CDLL(ctypes.util.find_library('c'))
+stderr_fileno = 2
+devnull = os.open(os.devnull, os.O_WRONLY)
+libc.dup2(devnull, stderr_fileno)
+# end clearing
+#function for continuous recording and recognition
+def record_continuous(language="en-US", mic_index=None, output_file="output.txt"):
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=mic_index)
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print("Listening started. Press Ctrl+C to stop.")
+
+        while True:
+            try:
+                audio = r.listen(source, timeout=None, phrase_time_limit=None)
+                try:
+                    text = r.recognize_google(audio, language=language)
+                    print(f"You said: {text}")
+                    #přehrtí
+                    subprocess.run(['espeak-ng', '-v', 'en-US', '-s 150', text])
+                    # Zápis do souboru
+                    with open(output_file, "a", encoding="utf-8") as f:
+                        f.write(text + "\n")
+
+                except sr.UnknownValueError:
+                    print("Neporozuměl jsem, zkuste znovu.")
+                except sr.RequestError as e:
+                    print(f"Chyba připojení k Google API: {e}")
+            except KeyboardInterrupt:
+                print("\nUkončuji poslouchání…")
+                sys.exit(0)
+
+if __name__ == "__main__":
+    record_continuous(language="en-US") #chenge depending on your language
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+#needed stuff to start the main file
+source /home/sebastian/repeat\ talker/.venv/bin/activate
+pip install -r requirements.txt
+sudo apt install espeak-ng
 python3 main.py
